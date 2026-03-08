b0932622936b2f33bb56925e3d8173b1ad1a57d7
Adi6tnine <yadavadarsh642@gmail.com>
2026-03-08 23:42:51 +0530

    cleanup: remove deployment scripts and test files from root

--- install-missing-deps.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing missing AWS SDK packages...
-cd backend
-call npm install @aws-sdk/client-textract @aws-sdk/client-polly @aws-sdk/client-transcribe-streaming @types/aws-lambda --save
-echo Done!
-pause
